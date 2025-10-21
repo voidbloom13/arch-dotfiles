9d47cd55f49743e36d3dc84497634cf04136e83e
xvoid <dakota.huyghe.dev@gmail.com>
2025-10-20 23:06:26 -0400

    Fixed typos (network-manager -> networkmanager, nameserver127.0.0.1 -> nameserver 127.0.0.1) in install.sh, added postgresql to install list, added postgresql setup section, and added colors

--- utils/scripts/install.sh
+++ utils/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ -d "$HOME/arch-dotfiles" ]] && [[ ! -d "$HOME/dotfiles" ]]; then
-  echo "Renaming arch-dotfiles to dotfiles..."
+  echo -e "\e[1;37mRenaming \e[1;31march-dotfiles \e[1;37mto \e[1;32mdotfiles\e[1;37m...\e[0m"
   mv $HOME/arch-dotfiles/ $HOME/dotfiles
 else
-  echo "Error with directory structure: Please ensure no other dotfiles folder in '$HOME'"
+  echo -e "\e[1;31mError with directory structure: \e[0;37mPlease ensure no other dotfiles folder in '\e[1;32m$HOME'\e[0m"
   exit 1
 fi
 
@@ -13,18 +13,17 @@ continue_install=${continue_install:-Y}
 
 case $continue_install in
   [yY] )
-    echo "Continuing installation..."
+    echo -e "\e[1;32mContinuing installation...\e[0m"
     ;;
   * )
-    echo "Aborting installation..."
+    echo -e "\e[1;31mAborting installation...\e[0m"
     exit 1
     ;;
 esac
 
 # Installs base packages
 cd ~
-sudo pacman -Syu aspnet-runtime base-devel blueman cifs-utils curl dotnet-runtime dotnet-sdk fastfetch fzf gcc ghostty git github-cli hypridle hyprlock hyprpaper jdk-openjdk kitty libreoffice-fresh man maven network-manager nm-connection-editor nvim nodejs npm obsidian ripgrep stow tmux tree ttf-font-awesome $(pacman -Sgq nerd-fonts) unzip waybar zoxide zsh
-# sudo pacman -S $(pacman -Sgq nerd-fonts) # Uncomment this line and remove from above line if error is thrown
+sudo pacman -Syu aspnet-runtime base-devel blueman cifs-utils curl dotnet-runtime dotnet-sdk fastfetch fzf gcc ghostty git github-cli hypridle hyprlock hyprpaper jdk-openjdk kitty libreoffice-fresh man maven networkmanager nm-connection-editor nvim nodejs npm obsidian postgresql ripgrep stow tmux tree ttf-font-awesome $(pacman -Sgq nerd-fonts) unzip waybar zoxide zsh
 
 # Clones and Installs yay
 git clone https://aur.archlinux.org/yay
@@ -57,7 +56,7 @@ fi
 
 if [[ -f /etc/resolv.conf ]]; then # replace these with your desired nameserver/s
   sudo mv /etc/resolv.conf /etc/resolv.conf.bak
-  echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver127.0.0.1" | sudo tee /etc/resolv.conf
+  echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 127.0.0.1" | sudo tee /etc/resolv.conf
 fi
 
 sudo systemctl disable --now systemd-networkd && sudo systemctl enable --now NetworkManager
@@ -66,7 +65,9 @@ sudo systemctl disable --now systemd-networkd && sudo systemctl enable --now Net
 source ~/dotfiles/utils/scripts/stow.sh
 cd && clear && fastfetch
 
+# Git Config and GH Authorization
 read -p "Setup git/github? [Y/n] " setup_git
+setup_git=${setup_git:-Y}
 case $setup_git in
   [yY] )
     read -p "Git config name: " git_name
@@ -79,4 +80,25 @@ case $setup_git in
     ;;
 esac
 
-echo -e "Next Steps:\n* Run [chsh] and change user shell to /usr/bin/zsh\n* Run [tmux] and press [<ctrl>+b, i] to install tmux plugins\n* Reboot"
+# PostgreSQL Setup
+echo -e "\n"
+read -p "Setup postgresql? [Y/n] " setup_pqsl
+setup_psql=${setup_psql:-Y}
+case $setup_psql in
+  [yY] )
+    read -p "Where do you want to initialize the database? Enter path from root (default /var/lib/postgres/data): " db_location
+    db_location=${db_location:-/var/lib/postgres/data}
+    echo -e "\e[0;37mInitializing DB at \e[1;34m$db_location\e[0m."
+    sudo -u postgres initdb -D "$db_location"
+    echo -e "\e[0;37mEnabling \e[1;34mpostgresql.service\e[0m."
+    sudo systemctl enable --now postgresql
+    echo -e "\e[0;37mCreating a new \e[1;34mPostgreSQL User\e[0m."
+    sudo -u postgres createuser --interactive
+    echo -e "\n\e[1;37mNext: Create a new database using \e[1;34m[createdb <\e[0;34mmyDatabaseName>]\e[0m\n"
+    ;;
+  * )
+    echo -e "To set up \e[1;34mPostgreSQL\e[0m, follow instructions in the \e]8;;https://wiki.archlinux.org/title/PostgreSQL\a\e[1;4;36mArch Wiki\e[0m\e]8;;\a"
+    ;;
+esac
+
+echo -e "\e[1;37mNext Steps\e[0m:\n* Run \e[1;31m[chsh]\e[0m and change user shell to \e[1;31m/usr/bin/zsh\e[0m\n* Run \e[1;32m[tmux]\e[0m and press \e[1;32m[<ctrl>+b, i]\e[0m to install tmux plugins\n* Reboot"
